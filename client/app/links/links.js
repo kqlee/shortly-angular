eb93e8a961258825cc766a9323db04ecfbfb3ff3
kqlee <kentqlee@gmail.com>
2016-05-21 11:38:50 -0700

    Implemented nav bar, enabled authentication

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,6 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
 
   $scope.data = {links: []};
+  //$scope.data.links = ['1', '2', '3'];
 
   Links.getAll(function (value) {
     $scope.data.links.push(value);
